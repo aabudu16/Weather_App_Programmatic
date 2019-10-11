7768ad7e5d6c92f629fad0077a9a158e9d2ee2ed
Mr Wonderful <mrwonderful@Mrs-MacBook-Pro.local>
2019-10-11 17:18:16 -0400

    Adds photo Model file and favroite Model file

---
+++ Weather_App_Programmatic/Models/PhotoModel.swift
@@ -0,0 +1,33 @@
+//
+//  PhotoModel.swift
+//  Pixabay-Photos-Save-Local
+//
+//  Created by Mr Wonderful on 10/1/19.
+//  Copyright © 2019 Mr Wonderful. All rights reserved.
+//
+
+
+import Foundation
+
+// MARK: - Picture
+struct Photos: Codable {
+    let totalHits: Int
+    let hits: [Hit]
+    let total: Int
+}
+// MARK: - Hit
+struct Hit: Codable {
+    let largeImageURL: String?
+    let webformatHeight, webformatWidth, likes, imageWidth: Int?
+    let id, userID, views, comments: Int?
+    let pageURL: String?
+    let imageHeight: Int?
+    let webformatURL: String?
+    let previewHeight: Int?
+    let tags: String?
+    let downloads: Int?
+    let user: String?
+    let favorites, imageSize, previewWidth: Int?
+    let userImageURL: String?
+    let previewURL: String?
+}
---
+++ Weather_App_Programmatic/Models/favoritePhotosModel.swift
@@ -0,0 +1,13 @@
+//
+//  favoritePhotosModel.swift
+//  Pixabay-Photos-Save-Local
+//
+//  Created by Mr Wonderful on 10/1/19.
+//  Copyright © 2019 Mr Wonderful. All rights reserved.
+//
+
+import Foundation
+
+struct FavoritePhotosModel:Codable{
+    let imageURL:String
+}
